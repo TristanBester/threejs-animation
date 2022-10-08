0cd8feb9dff32a10e60987bfc718739d6ec9e466
Tristan Bester <tristanbester@gmail.com>
2022-10-08 16:48:26 +0200

    Completed outer sphere

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 import * as THREE from 'three';
+import fragment from './shaders/fragment.glsl';
+import vertex from './shaders/vertex.glsl';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 
 export default class Sketch{
     constructor(){
@@ -7,27 +10,38 @@ export default class Sketch{
         this.renderer = new THREE.WebGLRenderer( { antialias: true } );
         this.renderer.setSize( window.innerWidth, window.innerHeight );
         document.getElementById('container').appendChild(this.renderer.domElement);
-
+        
         this.camera = new THREE.PerspectiveCamera( 70, window.innerWidth / window.innerHeight, 0.01, 10 );
-        this.camera.position.z = 1;
+        this.camera.position.set(0,0,1.3);
         this.scene = new THREE.Scene();
+        
+        this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
         this.addMesh();
         this.render();
     }
 
     addMesh(){
-        this.geometry = new THREE.PlaneGeometry( 1,1 );
-        this.material = new THREE.MeshNormalMaterial();
+        this.geometry = new THREE.SphereGeometry( 1.5, 32, 32 );
+        this.material = new THREE.MeshNormalMaterial({side: THREE.DoubleSide});
+        this.material = new THREE.ShaderMaterial({
+            fragmentShader: fragment,
+            vertexShader: vertex,
+            uniforms: {
+                time: { value: 0},
+            },
+            side: THREE.DoubleSide
+
+        })
 
         this.mesh = new THREE.Mesh( this.geometry, this.material );
         this.scene.add( this.mesh );
     }
 
     render(){
-        this.time++;
-        this.mesh.rotation.x += 0.01;
-	    this.mesh.rotation.y += 0.01;
+        this.time += 0.005
+        this.material.uniforms.time.value = this.time;
+
         this.renderer.render( this.scene, this.camera );
         window.requestAnimationFrame(this.render.bind(this))
     }
